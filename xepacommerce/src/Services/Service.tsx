fcf3729103dd536a3ea1bd97ca3fdef0c4aea85b
matheuscms95 <matheuscms95@gmail.com>
2022-06-02 15:40:32 -0300

    @matheuscms95 [Criando arquivo Services] {02/06/2022 | 15:39}

--- xepacommerce/src/Services/Service.tsx
+++ xepacommerce/src/Services/Service.tsx
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'https://xepacommerce.herokuapp.com'
 })
+
+export const login = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+}
