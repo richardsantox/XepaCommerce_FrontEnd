2b7e98002ee3522a4920b86763d8b2852e439848
richardsantox <richardsantospereira4322@gmail.com>
2022-06-05 01:02:04 -0300

    @richardsantox [Task: Pagina de contato]  {05/06/2022}

--- xepacommerce/src/Paginas/Contato/Contato.tsx
+++ xepacommerce/src/Paginas/Contato/Contato.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+
+import { Button, Grid, TextField } from '@material-ui/core';
+import { Box, Container } from '@mui/material';
+import "./Contato.css";
 
 function Contato() {
     return (
         <>
+            <Container >
+                <h1 className='titulo'>Entre em contato</h1>
+                <hr className='linha'/>
+
+                <Grid container alignItems='center'>
+                    <Grid md={6}>
+                        <form className='' >
+                            <TextField label="Nome" variant="outlined" fullWidth className='caixa' />
+                            <TextField label="Email" variant="outlined" fullWidth className='caixa' />
+                            <TextField label="Assunto" variant="outlined" fullWidth className='caixa' />
+                            <TextField label="Menssagem" className='caixa'
+                                fullWidth
+                                multiline
+                                rows={4}
+                                variant="outlined"
+                            />
+                            <Button type='submit' variant='contained' className='btnEnviar' fullWidth>
+                                Enviar
+                            </Button>
+                        </form>
+                    </Grid>
+                    <Grid md={6}>
+                        <Box paddingLeft={8}>
+                            <img src="https://i.imgur.com/fWAnFfN.gif" alt="" />
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
         </>
     );
 }
