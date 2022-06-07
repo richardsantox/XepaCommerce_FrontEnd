c9190ea1ebbffde7af2a3330cb05b23369806fa0
paulabrufato <paulabrufato.pb@gmail.com>
2022-06-07 16:18:37 -0300

    @paulabrufato [Task: Atualiação SobreNosl]{07/06/2022 | 16:18}

--- xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
+++ xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
@@ -10,7 +10,7 @@ function SobreNos() {
             <Box display="flex" flexDirection="row">
                 <Grid md={6}>
                     <Typography>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit blanditiis eveniet, labore doloremque laudantium odio placeat maiores commodi. Quia quas repudiandae laboriosam quae assumenda odio alias voluptates iusto mollitia eligendi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius in cupiditate exercitationem harum esse nam odit doloribus optio architecto, necessitatibus unde sequi voluptatibus velit ipsum officiis voluptatum hic? Error, alias.
+                        O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos. No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade. Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.   
                     </Typography>
                 </Grid>
                 <Grid md={6}>
@@ -26,7 +26,7 @@ function SobreNos() {
                 <Grid md={6}>
                     <h2>Missão</h2>
                     <Typography>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit blanditiis eveniet, labore doloremque laudantium odio placeat maiores commodi. Quia quas repudiandae laboriosam quae assumenda odio alias voluptates iusto mollitia eligendi?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure magnam blanditiis eum tenetur, voluptate perferendis optio molestias alias at! Quis cum assumenda repudiandae quo fugit alias eos magni officia ipsa.
                     </Typography>
                 </Grid>
             </Box>
@@ -35,7 +35,7 @@ function SobreNos() {
                 <Grid md={6}>
                     <h2>Visão</h2>
                     <Typography>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit blanditiis eveniet, labore doloremque laudantium odio placeat maiores commodi. Quia quas repudiandae laboriosam quae assumenda odio alias voluptates iusto mollitia eligendi?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem beatae excepturi in odit architecto delectus eaque voluptatum eius quas animi unde recusandae officiis mollitia, quo, nesciunt aliquid similique exercitationem culpa!
                     </Typography>
                 </Grid>
                 <Grid md={6}>
@@ -51,7 +51,7 @@ function SobreNos() {
                 <Grid md={6}>
                     <h2>Valores</h2>
                     <Typography>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit blanditiis eveniet, labore doloremque laudantium odio placeat maiores commodi. Quia quas repudiandae laboriosam quae assumenda odio alias voluptates iusto mollitia eligendi?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti eveniet at nulla culpa tenetur fugiat itaque temporibus ipsum atque, odio libero amet aliquam. Velit reprehenderit a error sunt aperiam excepturi.
                     </Typography>
                 </Grid>
                 
