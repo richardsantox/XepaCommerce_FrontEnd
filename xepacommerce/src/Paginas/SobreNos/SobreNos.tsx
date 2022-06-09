c0d884f6d1b5c361bf2c2e29d4aa5fd919ea6c8b
TheMires <thamiresfreitas051@gmail.com>
2022-06-09 15:25:12 -0300

    @TheMires [Adicionando imagens em sobre nós]{09/06/2022 -  15:24}

--- xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
+++ xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
@@ -14,14 +14,14 @@ function SobreNos() {
                         O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos. No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade. Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.   
                     </Typography>
                 </Grid>
-                <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+                <Grid md={6} display="flex" justifyContent="center">
+                <img src="https://i.imgur.com/Bt6CvxI.png" alt="" style={{ width: "60%" }} />
                 </Grid>
             </Box>
 
-            <Box display="flex" flexDirection="row">
-            <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+            <Box display="flex" flexDirection="row" >
+            <Grid md={6} display="flex" justifyContent="center">
+                <img src="https://i.imgur.com/TWV0VpK.png" alt="" style={{ width: "60%" }} />
                 </Grid>
 
                 <Grid md={6}>
@@ -39,14 +39,14 @@ function SobreNos() {
                     Visando a redução do desperdício de produtos perecíveis, com ciclos de consumo reduzidos, de modo a cooperar com as metas de desenvolvimento sustentáveis.
                     </Typography>
                 </Grid>
-                <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+                <Grid md={6} display="flex" justifyContent="center">
+                <img src="https://i.imgur.com/b0mWXhV.png" alt="" style={{ width: "60%" }} />
                 </Grid>
             </Box>
 
             <Box display="flex" flexDirection="row">
-            <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+            <Grid md={6} display="flex" justifyContent="center">
+                <img src="https://i.imgur.com/rGZSQ6P.png" alt="" style={{ width: "60%" }} />
                 </Grid>
 
                 <Grid md={6}>
