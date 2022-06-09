45bc50702e89b706e274d150cabaa9871e1e431f
matheuscms95 <matheuscms95@gmail.com>
2022-06-09 11:48:47 -0300

    @matheuscms95 [Task: Limpeza de código] {09/06/2022 | 11:48}

--- xepacommerce/src/Components/CardProduto/CardProduto.tsx
+++ xepacommerce/src/Components/CardProduto/CardProduto.tsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Typography, Card, CardActionArea, CardActions, CardContent, CardMedia, Button } from '@material-ui/core';
 import Batata from '../../assets/img/batata.png';
 import { Box } from '@mui/material';
 import './CardProduto.css';
 import { useNavigate } from 'react-router-dom';
 import Produto from '../../Modelos/ProdutoDTO';
-import { TokenState } from '../../Store/Tokens/TokensReducer';
-import { useSelector } from 'react-redux';
 import { busca } from '../../Services/Service';
-import useLocalStorage from 'react-use-localstorage';
 
 export default function Produtos() {
 
