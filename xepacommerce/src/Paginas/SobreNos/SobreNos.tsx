feddcfa28d65aeae82a172df0d2d14e69c4d1ceb
richardsantox <richardsantospereira4322@gmail.com>
2022-06-10 00:27:05 -0300

    @richardsantox [Atualizando Css Sobre nós]

--- xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
+++ xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
@@ -1,62 +1,68 @@
 import React from 'react';
 import { Box, Container, Grid, Typography } from '@mui/material';
 import './SobreNos.css';
+import Equipe from '../../Components/Equipe/Equipe';
 
 
 function SobreNos() {
     return (
 
-        <Container>
-            <h1 className='tituloSobreNos'>Xepa Commerce</h1>
-            <Box display="flex" flexDirection="row">
-                <Grid md={6}>
-                    <Typography className='sobreNos'>
-                        O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos. No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade. Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.   
+        <Container className="container-sobre">
+            
+            <Box className='box-sobre'>
+                <Grid md={6} className='sobreNos'>
+                <h1 className='tituloSobreNos'>Xepa Commerce</h1>
+                    <Typography >
+                        O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos.
+                        No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade.
+                        Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.
                     </Typography>
                 </Grid>
                 <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='primeira-img' />
                 </Grid>
             </Box>
-
-            <Box display="flex" flexDirection="row">
-            <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+            <hr className='linha'/>
+            <Box className='box-sobre' >
+                <Grid md={6}>
+                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
                 </Grid>
 
-                <Grid md={6}>
-                    <h2>Missão</h2>
-                    <Typography className='sobreNos'>
-                    Fortalecimento econômico de pequenos e médios agricultores utilizando a tecnologia para conectar o campo com os centros urbanos e pessoas em situação de vulnerabilidade.
+                <Grid md={6} className='sobreNos'>
+                    <h2 className='tituloSobreNos'>Missão</h2>
+                    <Typography >
+                        Fortalecimento econômico de pequenos e médios agricultores utilizando a tecnologia para conectar o campo com os centros urbanos e pessoas em situação de vulnerabilidade.
                     </Typography>
                 </Grid>
             </Box>
 
-            <Box display="flex" flexDirection="row">
-                <Grid md={6}>
-                    <h2>Visão</h2>
-                    <Typography className='sobreNos'>
-                    Visando a redução do desperdício de produtos perecíveis, com ciclos de consumo reduzidos, de modo a cooperar com as metas de desenvolvimento sustentáveis.
+            <Box className='box-sobre'>
+                <Grid md={6}className='sobreNos'>
+                    <h2 className='tituloSobreNos'>Visão</h2>
+                    <Typography >
+                        Visando a redução do desperdício de produtos perecíveis, com ciclos de consumo reduzidos, de modo a cooperar com as metas de desenvolvimento sustentáveis.
                     </Typography>
                 </Grid>
                 <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
                 </Grid>
             </Box>
 
-            <Box display="flex" flexDirection="row">
-            <Grid md={6}>
-                <img src="https://i.imgur.com/DRARIhi.gif" alt="" style={{ width: "50%" }} />
+            <Box className='box-sobre'>
+                <Grid md={6}>
+                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
                 </Grid>
 
-                <Grid md={6}>
-                    <h2>Valores</h2>
-                    <Typography className='sobreNos'>
-                    Proporcionar acessibilidade a produtos de qualidade e saúde. Com isso, geramos fortalecimento econômico de pequenos comerciantes e estimulamos uma produção consciente dos alimentos.
+                <Grid md={6} className='sobreNos'>
+                    <h2 className='tituloSobreNos'>Valores</h2>
+                    <Typography>
+                        Proporcionar acessibilidade a produtos de qualidade e saúde. Com isso, geramos fortalecimento econômico de pequenos comerciantes e estimulamos uma produção consciente dos alimentos.
                     </Typography>
                 </Grid>
-                
             </Box>
+            <hr />
+
+            <Equipe />
 
         </Container>
 
