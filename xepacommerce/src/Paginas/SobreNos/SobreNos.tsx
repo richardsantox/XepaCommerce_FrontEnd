e9f7cfcf341cc1b27d42372a5a41ba4889d22218
richardsantox <richardsantospereira4322@gmail.com>
2022-06-10 00:43:23 -0300

    @richardsantox [Atualizando Css Sobre nós]

--- xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
+++ xepacommerce/src/Paginas/SobreNos/SobreNos.tsx
@@ -7,109 +7,59 @@ import Equipe from '../../Components/Equipe/Equipe';
 function SobreNos() {
     return (
 
-<<<<<<< HEAD
-        <Container className="container-sobre">
-            
+
+        <Container className='container-sobre'>
+            <h1 className='titulos'>Xepa Commerce</h1>
             <Box className='box-sobre'>
                 <Grid md={6} className='sobreNos'>
-                <h1 className='tituloSobreNos'>Xepa Commerce</h1>
                     <Typography >
-                        O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos.
-                        No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade.
-                        Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.
-                    </Typography>
-                </Grid>
-                <Grid md={6}>
-                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='primeira-img' />
-                </Grid>
-            </Box>
-            <hr className='linha'/>
-            <Box className='box-sobre' >
-                <Grid md={6}>
-                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
-                </Grid>
-
-                <Grid md={6} className='sobreNos'>
-                    <h2 className='tituloSobreNos'>Missão</h2>
-                    <Typography >
-                        Fortalecimento econômico de pequenos e médios agricultores utilizando a tecnologia para conectar o campo com os centros urbanos e pessoas em situação de vulnerabilidade.
-=======
-        <Container>
-            <h1 className='titulos'>Xepa Commerce</h1>
-            <Box display="flex" flexDirection="row">
-                <Grid md={6}>
-                    <Typography className='sobreNos'>
                         O Xepa Commerce não é um E-commerce qualquer. Nossa meta é unir o melhor de dois mundos. No intuito de contibuir com pequenos agricultores locais, atrelado ao baixo custo que todo brasileiro adora, o Xepa quer garantir acesso àqueles em situação de vulnerabilidade, alimentos saudáveis e produtos de qualidade. Dessa forma, poupamos disperdícios, mitigando os impactos da insegurança alimentar na mesa dos brasileiros.
                     </Typography>
                 </Grid>
-                <Grid md={6} display="flex" justifyContent="center">
-                <img src="https://i.imgur.com/Bt6CvxI.png" alt="" style={{ width: "60%" }} />
+                <Grid md={6}>
+                    <img src="https://i.imgur.com/Bt6CvxI.png" alt="" className='primeira-img' />
                 </Grid>
             </Box>
-
-            <Box display="flex" flexDirection="row" >
-            <Grid md={6} display="flex" justifyContent="center">
-                <img src="https://i.imgur.com/TWV0VpK.png" alt="" style={{ width: "60%" }} />
+            <hr className='linha' />
+            <Box className='box-sobre'>
+                <Grid md={6}>
+                    <img src="https://i.imgur.com/TWV0VpK.png" alt="" className='imgs-sobre' />
                 </Grid>
 
-                <Grid md={6}>
-                    <h2 className='titulos'>Missão</h2>
-                    <Typography className='sobreNos'>
-                    Fortalecimento econômico de pequenos e médios agricultores utilizando a tecnologia para conectar o campo com os centros urbanos e pessoas em situação de vulnerabilidade.
->>>>>>> aa99d3e61dc76470f508668502dcd987044f0402
+                <Grid md={6} className='sobreNos'>
+                    <h1 className='titulos'>Missão</h1>
+                    <Typography >
+                        Fortalecimento econômico de pequenos e médios agricultores utilizando a tecnologia para conectar o campo com os centros urbanos e pessoas em situação de vulnerabilidade.
                     </Typography>
                 </Grid>
             </Box>
 
-<<<<<<< HEAD
+
             <Box className='box-sobre'>
-                <Grid md={6}className='sobreNos'>
-                    <h2 className='tituloSobreNos'>Visão</h2>
+                <Grid md={6} className='sobreNos'>
+                    <h1 className='titulos'>Visão</h1>
                     <Typography >
                         Visando a redução do desperdício de produtos perecíveis, com ciclos de consumo reduzidos, de modo a cooperar com as metas de desenvolvimento sustentáveis.
                     </Typography>
                 </Grid>
                 <Grid md={6}>
-                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
+                    <img src="https://i.imgur.com/b0mWXhV.png" alt="" className='imgs-sobre' />
                 </Grid>
             </Box>
 
             <Box className='box-sobre'>
                 <Grid md={6}>
-                    <img src="https://i.imgur.com/DRARIhi.gif" alt="" className='imgs-sobre' />
+                    <img src="https://i.imgur.com/rGZSQ6P.png" alt="" className='imgs-sobre' />
                 </Grid>
 
                 <Grid md={6} className='sobreNos'>
-                    <h2 className='tituloSobreNos'>Valores</h2>
-                    <Typography>
+                    <h1 className='titulos'>Valores</h1>
+                    <Typography >
                         Proporcionar acessibilidade a produtos de qualidade e saúde. Com isso, geramos fortalecimento econômico de pequenos comerciantes e estimulamos uma produção consciente dos alimentos.
-=======
-            <Box display="flex" flexDirection="row">
-                <Grid md={6}>
-                    <h2 className='titulos'>Visão</h2>
-                    <Typography className='sobreNos'>
-                    Visando a redução do desperdício de produtos perecíveis, com ciclos de consumo reduzidos, de modo a cooperar com as metas de desenvolvimento sustentáveis.
-                    </Typography>
-                </Grid>
-                <Grid md={6} display="flex" justifyContent="center">
-                <img src="https://i.imgur.com/b0mWXhV.png" alt="" style={{ width: "60%" }} />
-                </Grid>
-            </Box>
-
-            <Box display="flex" flexDirection="row">
-            <Grid md={6} display="flex" justifyContent="center">
-                <img src="https://i.imgur.com/rGZSQ6P.png" alt="" style={{ width: "60%" }} />
-                </Grid>
-
-                <Grid md={6}>
-                    <h2 className='titulos'>Valores</h2>
-                    <Typography className='sobreNos'>
-                    Proporcionar acessibilidade a produtos de qualidade e saúde. Com isso, geramos fortalecimento econômico de pequenos comerciantes e estimulamos uma produção consciente dos alimentos.
->>>>>>> aa99d3e61dc76470f508668502dcd987044f0402
                     </Typography>
                 </Grid>
             </Box>
-            <hr />
+            <hr className='linha'/>
 
             <Equipe />
 
