5416582997bb4064042f5029c73ea1dab1b67321
richardsantox <richardsantospereira4322@gmail.com>
2022-06-12 22:24:06 -0300

    @richardsantox [Task: Add fotos da equipe]  {12/06/2022}

--- xepacommerce/src/Components/Equipe/Equipe.tsx
+++ xepacommerce/src/Components/Equipe/Equipe.tsx
@@ -18,26 +18,26 @@ function Equipe() {
                             <CardMedia
                                 className=''
                                 component="img"
-                                alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
+                                alt="Ana Paula"
+                                image="https://i.imgur.com/vH6c8OR.jpg"
                                 title="Imagens dos integrantes do time do Xepa"
                             />
                             <CardActions className="card-icones">
                                 <Box className='redes-equipe'>
-                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                    <a href="#" target="_blank">
                                         <FacebookIcon className='icone-redes' />
                                     </a>
-                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                    <a href="#" target="_blank">
                                         <InstagramIcon className='icone-redes' />
                                     </a>
-                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                    <a href="#" target="_blank">
                                         <LinkedInIcon className='icone-redes' />
                                     </a>
                                 </Box>
                             </CardActions>
                             <CardContent >
                                 <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
+                                    Ana Paula Brufato
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
@@ -49,7 +49,7 @@ function Equipe() {
                                 className=''
                                 component="img"
                                 alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
+                                image="https://i.imgur.com/nEPf9lM.jpg"
                                 title="Imagens dos integrantes do time do Xepa"
                             />
                             <CardActions className="card-icones">
@@ -67,7 +67,127 @@ function Equipe() {
                             </CardActions>
                             <CardContent >
                                 <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
+                                    Richard Santos 
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                    <Card className='card-equipe'>
+                        <CardActionArea>
+                            <CardMedia
+                                className=''
+                                component="img"
+                                alt="Thamires"
+                                image="https://i.imgur.com/0DtkF13.jpg"
+                                title="Imagens dos integrantes do time do Xepa"
+                            />
+                            <CardActions className="card-icones">
+                                <Box className='redes-equipe'>
+                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                        <FacebookIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                        <InstagramIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                        <LinkedInIcon className='icone-redes' />
+                                    </a>
+                                </Box>
+                            </CardActions>
+                            <CardContent >
+                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
+                                    Thamires Aparecida
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                    <Card className='card-equipe'>
+                        <CardActionArea>
+                            <CardMedia
+                                className=''
+                                component="img"
+                                alt="Dannyela"
+                                image="https://i.imgur.com/WK9kTvT.jpg"
+                                title="Imagens dos integrantes do time do Xepa"
+                            />
+                            <CardActions className="card-icones">
+                                <Box className='redes-equipe'>
+                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                        <FacebookIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                        <InstagramIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                        <LinkedInIcon className='icone-redes' />
+                                    </a>
+                                </Box>
+                            </CardActions>
+                            <CardContent >
+                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
+                                    Dannyela Geovanna
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                    <Card className='card-equipe'>
+                        <CardActionArea>
+                            <CardMedia
+                                className=''
+                                component="img"
+                                alt="Lucas"
+                                image="https://i.imgur.com/mKZPDHC.jpg"
+                                title="Imagens dos integrantes do time do Xepa"
+                            />
+                            <CardActions className="card-icones">
+                                <Box className='redes-equipe'>
+                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                        <FacebookIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                        <InstagramIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                        <LinkedInIcon className='icone-redes' />
+                                    </a>
+                                </Box>
+                            </CardActions>
+                            <CardContent >
+                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
+                                    Lucas Reluz
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+
+                    <Card className='card-equipe'>
+                        <CardActionArea>
+                            <CardMedia
+                                className=''
+                                component="img"
+                                alt="Matheus"
+                                image="https://i.imgur.com/dsk2sDq.jpg"
+                                title="Imagens dos integrantes do time do Xepa"
+                            />
+                            <CardActions className="card-icones">
+                                <Box className='redes-equipe'>
+                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                        <FacebookIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                        <InstagramIcon className='icone-redes' />
+                                    </a>
+                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                        <LinkedInIcon className='icone-redes' />
+                                    </a>
+                                </Box>
+                            </CardActions>
+                            <CardContent >
+                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
+                                    Matheus Correia 
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
@@ -79,7 +199,7 @@ function Equipe() {
                                 className=''
                                 component="img"
                                 alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
+                                image="https://i.imgur.com/Mbbeg6Q.jpg"
                                 title="Imagens dos integrantes do time do Xepa"
                             />
                             <CardActions className="card-icones">
@@ -97,127 +217,7 @@ function Equipe() {
                             </CardActions>
                             <CardContent >
                                 <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-
-                    <Card className='card-equipe'>
-                        <CardActionArea>
-                            <CardMedia
-                                className=''
-                                component="img"
-                                alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
-                                title="Imagens dos integrantes do time do Xepa"
-                            />
-                            <CardActions className="card-icones">
-                                <Box className='redes-equipe'>
-                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                        <FacebookIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                        <InstagramIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                        <LinkedInIcon className='icone-redes' />
-                                    </a>
-                                </Box>
-                            </CardActions>
-                            <CardContent >
-                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-
-                    <Card className='card-equipe'>
-                        <CardActionArea>
-                            <CardMedia
-                                className=''
-                                component="img"
-                                alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
-                                title="Imagens dos integrantes do time do Xepa"
-                            />
-                            <CardActions className="card-icones">
-                                <Box className='redes-equipe'>
-                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                        <FacebookIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                        <InstagramIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                        <LinkedInIcon className='icone-redes' />
-                                    </a>
-                                </Box>
-                            </CardActions>
-                            <CardContent >
-                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-
-                    <Card className='card-equipe'>
-                        <CardActionArea>
-                            <CardMedia
-                                className=''
-                                component="img"
-                                alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
-                                title="Imagens dos integrantes do time do Xepa"
-                            />
-                            <CardActions className="card-icones">
-                                <Box className='redes-equipe'>
-                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                        <FacebookIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                        <InstagramIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                        <LinkedInIcon className='icone-redes' />
-                                    </a>
-                                </Box>
-                            </CardActions>
-                            <CardContent >
-                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-
-                    <Card className='card-equipe'>
-                        <CardActionArea>
-                            <CardMedia
-                                className=''
-                                component="img"
-                                alt="Richard"
-                                image="https://st2.depositphotos.com/6831718/11011/v/450/depositphotos_110113404-stock-illustration-user-cancel-icon-user-cancel.jpg"
-                                title="Imagens dos integrantes do time do Xepa"
-                            />
-                            <CardActions className="card-icones">
-                                <Box className='redes-equipe'>
-                                    <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                        <FacebookIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                        <InstagramIcon className='icone-redes' />
-                                    </a>
-                                    <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                        <LinkedInIcon className='icone-redes' />
-                                    </a>
-                                </Box>
-                            </CardActions>
-                            <CardContent >
-                                <Typography className="nome-equipe" gutterBottom variant="h5" component="h2">
-                                    Richard Santos
+                                    Vinicius Santos
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
